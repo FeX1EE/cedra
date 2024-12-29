e7daa72bb983c18b0930fef22b90695da2679f51
FeX1EE <iva_not_like@mail.ru>
2024-12-29 11:34:00 +0300

    last commit

--- mainScript.js
+++ mainScript.js
@@ -1,46 +1,5 @@
 let featuresList=document.querySelector(".FeatureBlocks")
-let featureList=[[`.body{
-    padding: 0;
-    margin: 0;
-}
-.FeatureBlock{
-    padding-top: 2vh;
-    display: flex;
-    flex-wrap: nowrap;
-    flex-direction: row;
-}
-.codeSample{
-    width: 30vw;
-    height: fit-content+10px;
-    justify-content: center;
-    margin-left: 5vw;
-    margin-right: 5vw;
-    padding: 5px;
-    border: solid 1px black;
-    background-color: gray;
-}
-.horLine{
-    padding: 0;
-    margin: 0;
-    width: 1px;
-    background-color: #000000;
-    size: 100%;
-    float: left;
-}
-.ExplText{
-    justify-content: center;
-    padding: 5px;
-    margin-left: 5vw;
-    margin-right: 5vw;
-}`,'explanation1'],['code2','explanation2'],['code3','explanation3'],['code4','explanation4'],['code5','explanation5'],['code6','explanation6']]
-
-// for (let i1 = 0; i1 < featureList.length; ++i1) {
-//     featuresList.innerHTML += `<div class="FeatureBlock">
-//     <code class="codeSample">${featureList[i1][0]}</code>
-//     <hr class="horLine" width="2" size="100%">
-//     <h4 class="ExplText">${featureList[i1][1]}</h4>
-//     </div>`
-// }
+let featureList=[['code1','explanation1'],['code2','explanation2'],['code3','explanation3'],['code4','explanation4'],['code5','explanation5'],['code6','explanation6']]
 
 for (let i1 = 0; i1 < featureList.length; i1++) {
     let block = document.createElement("div")
