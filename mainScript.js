a9513c5e2ddbd15e52599ae1ec4c5b16ed4ab648
FeX1EE <iva_not_like@mail.ru>
2025-01-19 11:36:54 +0300

    123

--- mainScript.js
+++ mainScript.js
@@ -1,7 +1,27 @@
 let featuresList=document.querySelector(".FeatureBlocks")
-let featureList=[['code1\nforx','explanation1'],['code2\nforx','explanation2'],['code3\nforx','explanation3'],['code4','explanation4'],['code5','explanation5'],['code6','explanation6'],['code7\nforx','explanation7'],['code8','explanation8'],['code9','explanation9'],['code10','explanation10']]
 let mainBut = document.querySelector(".toMainBut")
 let randBut = document.querySelector(".RandomBut")
+// let featureList=[['code1\nforx','explanation1'],['code2\nforx','explanation2'],['code3\nforx','explanation3'],['code4','explanation4'],['code5','explanation5'],['code6','explanation6'],['code7\nforx','explanation7'],['code8','explanation8'],['code9','explanation9'],['code10','explanation10']]
+let featureList=[]
+let LStL=JSON.parse(localStorage.getItem("codes")).length
+console.log(LStL)
+if(LStL==0)
+{
+    localStorage.setItem("codes", JSON.stringify(['code1\nforx','code2\nforx','code3\nforx','code4','code5','code6','code7\nforx','code8','code9','code10']))
+    localStorage.setItem("explanations", JSON.stringify(['explanation1','explanation2','explanation3','explanation4','explanation5','explanation6','explanation7','explanation8','explanation9','explanation10']))
+    featureList=[['code1\nforx','explanation1'],['code2\nforx','explanation2'],['code3\nforx','explanation3'],['code4','explanation4'],['code5','explanation5'],['code6','explanation6'],['code7\nforx','explanation7'],['code8','explanation8'],['code9','explanation9'],['code10','explanation10']]
+}
+else
+{
+    let r1=JSON.parse(localStorage.getItem("codes"))
+    let r2=JSON.parse(localStorage.getItem("explanations"))
+    for(let i1=0;i1<LStL;++i1)
+    {
+        featureList.push([r1[i1],r2[i1]])
+    }
+}
+console.log(featureList)
+
 for (let i1 = 0; i1 < featureList.length; i1++) {
     let block = document.createElement("div")
     block.classList.add("FeatureBlock")
@@ -20,7 +40,6 @@ for (let i1 = 0; i1 < featureList.length; i1++) {
 }
 mainBut.addEventListener("click",function(){
     featuresList.replaceChildren('')
-    console.log("mainBut")
     for (let i1 = 0; i1 < featureList.length; i1++) {
         let block = document.createElement("div")
         block.classList.add("FeatureBlock")
@@ -38,10 +57,16 @@ mainBut.addEventListener("click",function(){
         block.appendChild(h4)
     }
 })
+console.log(Math.random())
+let Nbt=0
 randBut.addEventListener("click",function(){
     featuresList.replaceChildren('')
-    console.log("randBut")
-    let Rnum = Math.floor(Math.random() * featureList.length)
+    let Rnum = Math.floor((Math.random() * (featureList.length-1)))
+    if(Rnum>=Nbt)
+    {
+        Rnum+=1
+    }
+    Nbt=Rnum
     let block = document.createElement("div")
     block.classList.add("FeatureBlock")
     featuresList.appendChild(block)
