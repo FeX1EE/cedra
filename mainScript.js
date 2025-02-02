5f9ecb91dc6a2337f52df7769141906bc2bedce0
FeX1EE <iva_not_like@mail.ru>
2025-02-02 17:22:37 +0300

    initcom

--- mainScript.js
+++ mainScript.js
@@ -3,9 +3,7 @@ let mainBut = document.querySelector(".toMainBut")
 let randBut = document.querySelector(".RandomBut")
 // let featureList=[['code1\nforx','explanation1'],['code2\nforx','explanation2'],['code3\nforx','explanation3'],['code4','explanation4'],['code5','explanation5'],['code6','explanation6'],['code7\nforx','explanation7'],['code8','explanation8'],['code9','explanation9'],['code10','explanation10']]
 let featureList=[]
-let LStL=JSON.parse(localStorage.getItem("codes")).length
-console.log(LStL)
-if(LStL==0)
+if(localStorage.getItem("codes")==null)
 {
     localStorage.setItem("codes", JSON.stringify(['code1\nforx','code2\nforx','code3\nforx','code4','code5','code6','code7\nforx','code8','code9','code10']))
     localStorage.setItem("explanations", JSON.stringify(['explanation1','explanation2','explanation3','explanation4','explanation5','explanation6','explanation7','explanation8','explanation9','explanation10']))
@@ -13,6 +11,7 @@ if(LStL==0)
 }
 else
 {
+    let LStL=JSON.parse(localStorage.getItem("codes")).length
     let r1=JSON.parse(localStorage.getItem("codes"))
     let r2=JSON.parse(localStorage.getItem("explanations"))
     for(let i1=0;i1<LStL;++i1)
@@ -20,16 +19,23 @@ else
         featureList.push([r1[i1],r2[i1]])
     }
 }
-console.log(featureList)
-
 for (let i1 = 0; i1 < featureList.length; i1++) {
     let block = document.createElement("div")
     block.classList.add("FeatureBlock")
     featuresList.appendChild(block)
+    let blockCode = document.createElement("div")
+    blockCode.classList.add("FeatureBlockCode")
+    block.appendChild(blockCode)
     let code = document.createElement("code")
     code.classList.add("codeSample")
     code.innerHTML=featureList[i1][0]
-    block.appendChild(code)
+    blockCode.appendChild(code)
+    let copyBut = document.createElement("div")
+    copyBut.classList.add("copyBut")
+    copyBut.addEventListener("click",function(){
+        navigator.clipboard.writeText(code.innerHTML)
+    })
+    blockCode.appendChild(copyBut)
     let hr = document.createElement("hr")
     hr.classList.add("horLine")
     block.appendChild(hr)
@@ -44,10 +50,19 @@ mainBut.addEventListener("click",function(){
         let block = document.createElement("div")
         block.classList.add("FeatureBlock")
         featuresList.appendChild(block)
+        let blockCode = document.createElement("div")
+        blockCode.classList.add("FeatureBlockCode")
+        block.appendChild(blockCode)
         let code = document.createElement("code")
         code.classList.add("codeSample")
         code.innerHTML=featureList[i1][0]
-        block.appendChild(code)
+        blockCode.appendChild(code)
+        let copyBut = document.createElement("div")
+        copyBut.classList.add("copyBut")
+        copyBut.addEventListener("click",function(){
+            navigator.clipboard.writeText(code.innerHTML)
+        })
+        blockCode.appendChild(copyBut)
         let hr = document.createElement("hr")
         hr.classList.add("horLine")
         block.appendChild(hr)
@@ -57,7 +72,6 @@ mainBut.addEventListener("click",function(){
         block.appendChild(h4)
     }
 })
-console.log(Math.random())
 let Nbt=0
 randBut.addEventListener("click",function(){
     featuresList.replaceChildren('')
@@ -70,10 +84,19 @@ randBut.addEventListener("click",function(){
     let block = document.createElement("div")
     block.classList.add("FeatureBlock")
     featuresList.appendChild(block)
+    let blockCode = document.createElement("div")
+    blockCode.classList.add("FeatureBlockCode")
+    block.appendChild(blockCode)
     let code = document.createElement("code")
     code.classList.add("codeSample")
     code.innerHTML=featureList[Rnum][0]
-    block.appendChild(code)
+    blockCode.appendChild(code)
+    let copyBut = document.createElement("div")
+    copyBut.classList.add("copyBut")
+    copyBut.addEventListener("click",function(){
+        navigator.clipboard.writeText(code.innerHTML)
+    })
+    blockCode.appendChild(copyBut)
     let hr = document.createElement("hr")
     hr.classList.add("horLine")
     block.appendChild(hr)
